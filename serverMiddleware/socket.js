fc6d72b9ef50dc54e824205df54fb2a5c81200d3
Tuan Bui <ntuanb@gmail.com>
2021-09-10 11:58:56 +1000

    Test

--- serverMiddleware/socket.js
+++ serverMiddleware/socket.js
@@ -1,10 +1,18 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-
-const io = require("socket.io")(3001, {
-  cors: {
-    origin: '*',
-  },
-  transports: ['websocket']
+app.get('/', (req, res) => {
+  res.json('success')
 });
 
-export default io
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3001, () => {
+  console.log('listening on *:3001');
+});
