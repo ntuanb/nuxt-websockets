0f3b4ef1edd026e5c7d31294d0a04b2ef4d76931
Tuan Bui <ntuanb@gmail.com>
2021-09-10 12:00:56 +1000

    Test

--- serverMiddleware/event.js
+++ serverMiddleware/event.js
@@ -1,30 +1,4 @@
-export {}
-
- 
-
-const bodyParser = require('body-parser')
-
-const app = require('express')()
-
- 
-
-app.use(bodyParser.json())
-
- 
-
-const io = require("socket.io")(3001, {
-  cors: {
-    origin: '*',
-  },
-  transports: ['websocket']
-});
-
- io.on('connect', () => {
-   console.log('connected')
- })
- io.on('message', (d) => {
-   console.log(d)
- })
+import socket from './socket'
 
 app.all('/', async (req, res) => {
 
@@ -32,14 +6,10 @@ app.all('/', async (req, res) => {
 
  
 
-  io.emit('event', req.body);
+  socket.emit('event', req.body);
 
  
 
   res.json('Success')
 
 });
-
- 
-
-module.exports = app
--- serverMiddleware/socket.js
+++ serverMiddleware/socket.js
@@ -3,7 +3,12 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
 
 app.get('/', (req, res) => {
   res.json('success')
@@ -13,6 +18,12 @@ io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
+io.on('message', (msg) => {
+  console.log(msg);
+});
+
 server.listen(3001, () => {
   console.log('listening on *:3001');
 });
+
+export default io
